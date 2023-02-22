eb78634c77de369d56f21edec994a649b57f25c5
branhickman <branhickman@gmail.com>
2023-02-22 13:37:40 -0700

    setup models via sequelize in bit.io

---
+++ models/listing.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Listing extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Listing.init({
+    itemName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photos: DataTypes.ARRAY(DataTypes.STRING),
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    condition: {
+      type: DataTypes.ENUM('Like New', 'Fair', 'Poor'),
+      defaultValue: 'Poor'
+    },
+    openToTrade: DataTypes.BOOLEAN,
+    price: DataTypes.INTEGER,
+    profileId: {
+      type: DataTypes.INTEGER,
+			allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id'
+      }
+  }
+  }, {
+    sequelize,
+    modelName: 'Listing',
+  });
+  return Listing;
+};
